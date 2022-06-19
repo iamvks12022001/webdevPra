f14b7c8235557ce32ef9ce4102c2223e397ac7b7
vikash <vikashkumar754189@gmail.com>
2022-06-19 14:14:40 +0530

    connect to db

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 8000;
-
+const expresslayout = require("express-ejs-layouts");
+const db = require("./config/mongoose");
 app.set("view engine", "ejs");
 //app.set("views", "./views");
+
+app.use(expresslayout);
+app.use(express.static("./assets"));
+
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
 app.use("/", require("./route"));
 
 app.listen(port, function (err) {
